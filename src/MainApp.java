7923fc5349c198c3e06f5391626908139343d0bc
Dexter <kastrullkastaren@hotmail.coim>
2017-08-11 13:28:45 +0200

    Added all files

---
+++ src/MainApp.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Lukas on 10/08/2017.
+ */
+public class MainApp extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Menu/Menu.fxml"));
+
+        primaryStage.setTitle("Sagan om SpoderJW");
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(new Scene(loader.load()));
+
+        primaryStage.show();
+    }
+}
---
+++ src/Menu/Controller.java
@@ -0,0 +1,28 @@
+package Menu;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    public Button startButton;
+
+    @FXML
+    public Button optionsButton;
+
+    @FXML
+    public Button exitButton;
+
+    public void handleOnStart() {
+        System.out.println("Start");
+    }
+
+    public void handleOnOptions() {
+        System.out.println("Options");
+    }
+
+    public void handleOnExit() {
+        System.out.println("Exit");
+    }
+}
